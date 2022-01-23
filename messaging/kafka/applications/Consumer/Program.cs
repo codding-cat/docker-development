ffb5c54b3367815662279681e46ca1886709ed3d
codding-cat <tom.cat.cocaine@gmail.com>
2022-01-23 20:30:27 +0300

    Consumer on c#

---
+++ messaging/kafka/applications/Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using Consumer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+CreateHostBuilder(args).Build().Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddHostedService<Worker>();
+        });
