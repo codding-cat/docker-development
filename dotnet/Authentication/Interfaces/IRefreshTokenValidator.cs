3087912b08cd21a9c069f26e1f5cb4b347e20d92
Codding Cat <tom.cat.cocaine@gmail.com>
2022-01-25 12:38:47 +0300

    refresh token validator

---
+++ dotnet/Authentication/Interfaces/IRefreshTokenValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace Authentication.Interfaces;
+
+/// <summary>
+/// Interface for validating refresh token.
+/// </summary>
+public interface IRefreshTokenValidator
+{
+    /// <summary>
+    /// Validates refresh token.
+    /// </summary>
+    /// <param name="refreshToken">The refresh token.</param>
+    /// <returns>True if token is valid,otherwise false.</returns>
+    bool Validate(string refreshToken);
+}
