fc77afe90fc6a2906ab544c46690d3b40abe6ac5
Codding Cat <tom.cat.cocaine@gmail.com>
2022-01-25 15:03:59 +0300

    commenting a little bit

--- dotnet/Authentication/Services/UsersService.cs
+++ dotnet/Authentication/Services/UsersService.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace Authentication.Services;
 
+/// <summary>
+/// Users service
+/// </summary>
 public class UsersService: IUsersService
 {
     private readonly IUsersRepository _usersRepository;
@@ -25,11 +28,21 @@ public class UsersService: IUsersService
         _refreshTokenService = refreshTokenService;
     }
     
+    /// <summary>
+    /// Get all users async
+    /// </summary>
+    /// <returns></returns>
     public async Task<IEnumerable<User>> GetAllAsync()
     {
         return await _usersRepository.GetAllAsync();
     }
 
+    /// <summary>
+    /// Login async
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    /// <exception cref="AuthenticationException"></exception>
     public async Task<(bool success, string accessToken, string refreshToken)> LoginAsync(LoginData data)
     {
         var user = await _usersRepository.GetByNameAsync(data.Name);
@@ -51,6 +64,13 @@ public class UsersService: IUsersService
         return (true, accessToken, refreshToken);
     }
 
+    /// <summary>
+    /// Create user
+    /// </summary>
+    /// <param name="user"></param>
+    /// <returns></returns>
+    /// <exception cref="DataConflictException"></exception>
+    /// <exception cref="Exception"></exception>
     public async Task<User> CreateAsync(User user)
     {
         var existingUser = await _usersRepository.GetByNameAsync(user.Name);
