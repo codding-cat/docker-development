acbbd7b2b27a7e89f983df451fb7bcc341763118
Codding Cat <tom.cat.cocaine@gmail.com>
2022-01-25 12:33:34 +0300

    token generators services

---
+++ dotnet/Authentication/Interfaces/ITokenService.cs
@@ -0,0 +1,20 @@
+﻿using Authentication.Models;
+
+namespace Authentication.Interfaces;
+
+/// <summary>
+/// Interface for generating token.
+/// </summary>
+public interface ITokenService
+{
+    /// <summary>
+    /// Generates token based on user information.
+    /// </summary>
+    /// <param name="user"><see cref="User"/> instance.</param>
+    /// <returns>Generated token.</returns>
+    string Generate(User user);
+}
+
+public interface IRefreshTokenService : ITokenService { }
+
+public interface IAccessTokenService : ITokenService { }
--- dotnet/Authentication/Models/JwtSettings.cs
+++ dotnet/Authentication/Models/JwtSettings.cs
@@ -4,8 +4,8 @@ public class JwtSettings
 {
     public string AccessTokenSecret { get; set; } = "";
     public string RefreshTokenSecret { get; set; } = "";
-    public double AccessTokenExpirationMinutes { get; set; }
-    public double RefreshTokenExpirationMinutes { get; set; }
+    public int AccessTokenExpirationSeconds { get; set; }
+    public int RefreshTokenExpirationSeconds { get; set; }
     public string Issuer { get; set; } = "";
     public string Audience { get; set; } = "";
 }
